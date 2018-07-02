4ca8276e25056f86a9736a040f40b48ed2baf98e
MasterKingRR <40745186+MasterKingRR@users.noreply.github.com>
2018-07-01 21:08:45 -0300

    Update bot_tutorial_welcome_6.py

--- bot_tutorial_welcome_6.py
+++ bot_tutorial_welcome_6.py
@@ -1,12 +1,12 @@
 @client.event
 async def on_member_join(member):
-  canal = client.get_channel("423328604911304708")
-  regras = client.get_channel("420612829838573588")
-  msg = "Bem Vindo {}\n leia as {}".format(member.mention, regras.mention)
+  canal = client.get_channel("462791105771995158")
+  regras = client.get_channel("463111133096574987")
+  msg = "Bem Vindo {}\n reuniões:{}".format(member.mention, regras.mention)
   await client.send_message(canal, msg) #substitua canal por member para enviar a msg no DM do membro
 
 @client.event
 async def on_member_remove(member):
    canal = client.get_channel("423328604911304708")
-   msg = "Adeus garotinho juvenil {}".format(member.mention)
+   msg = "Adeus{}".format(member.mention)
    await client.send_message(canal, msg) #substitua canal por member para enviar a msg no DM do membro
